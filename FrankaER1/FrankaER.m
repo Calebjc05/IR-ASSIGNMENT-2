5fecc955613e614f77a13dca3d716a539e17afa6
Caleb Chadwick <caleb.chadwick1@icloud.com>
2024-10-30 08:59:55 +1100

    Final

--- FrankaER1/FrankaER.m
+++ FrankaER1/FrankaER.m
@@ -20,6 +20,7 @@ classdef FrankaER < RobotBaseClass
             link(5) = Link('d', 0.384, 'a', 0.0825,'alpha', pi/2, 'offset',0);
             link(6) = Link('d', 0.0000, 'a', 0.0000, 'alpha', pi/2, 'offset',0);
             link(7) = Link('d', 0.107, 'a', 0.0880, 'alpha', 0.00, 'offset',0);
+            link(2).qlim = [deg2rad(-30) deg2rad(30)];
             self.model = SerialLink(link,'name',self.name);
         end   
     end
